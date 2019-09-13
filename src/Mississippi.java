0bad694ab0c23da5e3bff3a87865b30d1d6e56b1
do_tha000 <do_tha000@CAM-LAB-12.bgsd.k12.wa.us>
2019-09-13 07:44:06 -0700

    Mississipi Project

--- src/Mississippi.java
+++ src/Mississippi.java
@@ -3,7 +3,22 @@ public class Mississippi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		M();
+		I();
+		S();
+		S();
+		I();
+		S();
+		S();
+		I();
+		P();
+		P();
+		I();
+	}
+	public static void M {
+		System.out.println("M     M");
+		System.out.println("MM   MM");
+		System.out.println("M M M M");
 	}
 
 }
